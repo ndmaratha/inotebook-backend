0636855dd470cfbe49eabf3f3de638258de2c629
Nayan Desai <ndmaratha01@gmail.com>
2023-03-04 12:33:15 +0530

    backend Completed

--- Routes/notes.js
+++ Routes/notes.js
@@ -3,13 +3,17 @@ const { body, validationResult } = require("express-validator");
 
 const Note = require("../models/Note");
 const fetchUser = require("../Middleware/fetchUser");
+const { findByIdAndUpdate } = require("../models/Note");
 const router = express.Router();
 
+
+//first Route1 fetch all notes login required
 router.get("/fetchallnotes", fetchUser, async (req, res) => {
 	const notes = await Note.find({ user: req.user.id });
 	res.json(notes);
 });
 
+// Route2  add notes login required
 router.post(
 	"/addnote",
 	fetchUser,
@@ -43,4 +47,62 @@ router.post(
 		}
 	}
 );
+
+// Route3  update notes login required
+
+router.put("/updatenote/:id", fetchUser, async (req, res) => {
+	try {
+		const { title, description, tag } = req.body;
+		const newNote = {};
+		if (title) {
+			newNote.title = title;
+		}
+		if (description) {
+			newNote.description = description;
+		}
+		if (tag) {
+			newNote.tag = tag;
+		}
+		let note =await Note.findById(req.params.id);
+		if (!note) {
+			return res.status(404).send("not Found");
+		}
+		if (note.user.toString() != req.user.id) {
+			return res.status(401).send("not allwed");
+		}
+		note = await Note.findByIdAndUpdate(
+			req.params.id,
+			{ $set: newNote },
+			{ new: true }
+		);
+		
+		res.json(note);
+	} catch (error) {
+		//if some strang happned
+		console.log("something error", error);
+		res.status(500).send("some Error occures");
+	}
+});
+// Route4  delete notes login required
+
+router.delete("/deletenote/:id", fetchUser, async (req, res) => {
+	try {
+		
+		
+		let note =await Note.findById(req.params.id);
+		if (!note) {
+			return res.status(404).send("not Found");
+		}
+		if (note.user.toString() != req.user.id) {
+			return res.status(401).send("not allwed");
+		}
+		
+		note=await Note.findByIdAndDelete(req.params.id)
+		res.json({"successfully" :"deleted",note:note});
+	} catch (error) {
+		//if some strang happned
+		console.log("something error", error);
+		res.status(500).send("some Error occures");
+	}
+});
 module.exports = router;
