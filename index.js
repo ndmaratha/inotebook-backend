c32ae02bcd6f2423cb93f7e74af258931eed4e1c
Nayan Desai <ndmaratha01@gmail.com>
2023-03-23 19:53:18 +0530

    cors required things added in index.js

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const connectTomongoose = require("./database");
 console.log("server is started");
 connectTomongoose();
+const cors = require("cors");
 const express = require("express");
 const app = express();
 const port = 5000;
 
+app.use(cors());
 app.use(express.json());
 app.use("/api/auth/", require("./Routes/auth"));
 app.use("/api/notes/", require("./Routes/notes"));
